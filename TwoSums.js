b5ec093c4c9124b24bc7e34f5b39d623e2612444
berlink2 <berlink.k2@gmail.com>
2020-09-20 00:08:01 +0700

    solved two sums

---
+++ TwoSums.js
@@ -0,0 +1,37 @@
+// Two Sums
+// Given an array of integers nums and an integer target, return indices of the two numbers
+// such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use
+// the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 13
+// Output: [0,2]
+// Output: Because nums[0] + nums[2] == 13, we return [0, 2].
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+const TwoSums = (nums,target) =>{
+    const length = nums.length;
+    const count = new Map();
+    for (let i=0;i<length;i++) {
+        const complement = target - nums[i];
+        if (count.has(complement)) {
+            return [count.get(complement),i]
+        } else {
+            count.set(nums[i], i);
+        }
+    }
+    return -1;
+}
+
+console.log(TwoSums([2,7,11,15], 13));
+console.log(TwoSums([3,2,4], 6));
+console.log(TwoSums([3,3], 6));
