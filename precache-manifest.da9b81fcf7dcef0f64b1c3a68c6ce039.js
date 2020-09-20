e17e8de3778f64819d7884b0a62ec1a608358039
berlink2 <berlink.k2@gmail.com>
2020-09-20 16:55:43 +0700

    Updates

---
+++ precache-manifest.da9b81fcf7dcef0f64b1c3a68c6ce039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9398637064164a2714a93d01426dbff",
+    "url": "/mcpTest/index.html"
+  },
+  {
+    "revision": "c84429dbda90b64114b8",
+    "url": "/mcpTest/static/css/main.f02ba3f7.chunk.css"
+  },
+  {
+    "revision": "954b03c35224e8344c86",
+    "url": "/mcpTest/static/js/2.c8502f23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mcpTest/static/js/2.c8502f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84429dbda90b64114b8",
+    "url": "/mcpTest/static/js/main.be1d728c.chunk.js"
+  },
+  {
+    "revision": "c5ff6333e1bcf53c01ee",
+    "url": "/mcpTest/static/js/runtime-main.97689e0c.js"
+  },
+  {
+    "revision": "4e4bca50a2c4035b769763528dc6a257",
+    "url": "/mcpTest/static/media/mcpayment-logo.4e4bca50.png"
+  }
+]);
